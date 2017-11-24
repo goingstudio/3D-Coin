c52af4600f298aed51a3cd1c49b4fbd4399f392d
Treesoon\DAFT <treesoon.dev@goingstudio.com>
2017-11-24 19:58:25 +0700

    เหลือ menu-responsive ครับพี่

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,27 @@
+
+
+
 jQuery(document).ready(function($) {
+    new WOW({
+        scrollContainer: '#page'
+    }).init();
 
     //Slidebars
-    var controller = new slidebars();
-    controller.init();
+    // var controller = new slidebars();
+    // controller.init();
 
-    // Toggle Slidebars
-    $('.toggle-menu').on('click', function(event) {
-        // Stop default action and bubbling
-        event.stopPropagation();
-        event.preventDefault();
+    // // Toggle Slidebars
+    // $('.toggle-menu').on('click', function(event) {
+    //     // Stop default action and bubbling
+    //     event.stopPropagation();
+    //     event.preventDefault();
 
-        // Toggle the Slidebar with id 'id-1'
-        controller.toggle('page-sidebar');
-    });
+    //     // Toggle the Slidebar with id 'id-1'
+    //     controller.toggle('page-sidebar');
+    // });
+
+
+   
+
 
 });
